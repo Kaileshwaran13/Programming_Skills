f6fc107c52b4a40ab82d339d03ca9beb1a7951e2
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-02-26 15:04:54 +0530

    Add files via upload

---
+++ Basic Implementation/Merge Strings Alternately/mergeStringsAlternately.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result;
+        int maxSize = max(word1.size(), word2.size());
+        for(int i = 0; i < maxSize; i++){
+            if( i < word1.size()){
+                result += word1[i];
+            }
+            if( i < word2.size() ){
+                result += word2[i];
+            }
+        }
+        return result;
+    }
+};
